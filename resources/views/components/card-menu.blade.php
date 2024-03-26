5173cf108232f486445aaf279aa6966fd7ac50ac
SywlAchmd <syawalachmad1323@gmail.com>
2024-03-27 00:41:53 +0800

    add card menu and category component

---
+++ resources/views/components/card-menu.blade.php
@@ -0,0 +1,16 @@
+<section class="flex w-fit flex-col gap-3 bg-[#F3F3F3] px-2 py-4">
+  <img
+    src="https://www.bifolcomatty.co.uk/wp-content/uploads/2019/08/placeholder-square.jpg"
+    alt=""
+    class="h-20 w-32 rounded-xl object-cover"
+  />
+
+  <div class="max-w-32">
+    <h5 class="text-sm">{{ $name ?? "Nama Menu" }}</h5>
+    <p class="line-clamp-2 text-[10px]">
+      {{ $description ?? "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel consectetur quas saepe ullam non facilis adipisci nesciunt ex officiis nihil." }}
+    </p>
+  </div>
+
+  <p class="text-sm font-semibold">{{ $price ?? "Rp.20.000" }}</p>
+</section>
---
+++ resources/views/components/category.blade.php
@@ -0,0 +1,7 @@
+<section class="flex flex-col items-center gap-1">
+  <div class="flex h-6 w-6 items-center justify-center rounded-full bg-[#D9D9D9] p-4">
+    <i class="{{ $icon }}"></i>
+  </div>
+
+  <p class="text-xs font-semibold">{{ $name }}</p>
+</section>
