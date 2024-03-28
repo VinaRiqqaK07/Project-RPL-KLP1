8a477f24507d4d571021fe8bcb0fbba862893774
VinaRiqqaK07 <vinariqqakhairiah@gmail.com>
2024-03-28 17:01:33 +0800

    add make order modal component

---
+++ resources/views/components/make-order-modal.blade.php
@@ -0,0 +1,16 @@
+<section class="w-full h-full bg-gray-500">
+    <footer class="h-auto w-full rounded-t-xl bg-slate-50 p-4 fixed bottom-0 left-0">
+        <section class="flex flex-row items-center justify-between mb-4 mt-1">
+            <p class="font-bold text-lg">Identitas Pemesanan</p>
+            <button id="closeModal"><i class="fa-solid fa-circle-xmark fa-xl"></i></button>
+            
+        </section>
+        <section class="flex flex-col my-1 pb-2 gap-2">
+            <label class="font-semibold my-1">Nama Pemesan</label>
+            <input type="text" placeholder="Masukkan nama Anda" class="px-2 py-3 focus:outline-none focus:ring focus:border-blue-300 rounded-lg bg-slate-100 " />
+            <label class="font-semibold mt-3">Nomor Meja</label>
+            <input type="text" placeholder="Masukkan nomor meja" class="px-2 py-3 focus:outline-none focus:ring focus:border-blue-300 rounded-lg bg-slate-100 " />
+            <button class="w-full py-3 px-4 mt-4 text-white text-md font-semibold bg-[#70B44E] rounded-lg" >Pesan</button>
+        </section>   
+    </footer>
+</section>
