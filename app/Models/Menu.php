5120979341f18e3fc284a92f1af98170185e4dae
SywlAchmd <syawalachmad1323@gmail.com>
2024-04-17 23:09:02 +0800

    feat: create relation between discount and menu

--- app/Models/Menu.php
+++ app/Models/Menu.php
@@ -5,21 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Menu extends Model
+class Menu extends Model implements HasMedia
 {
-    use HasFactory;
-
-    // protected $fillable = [
-    //     'category_id',
-    //     'discount_id',
-    //     'name',
-    //     'price',
-    //     'quantity',
-    //     'status',
-    //     'description',
-    //     'image',
-    // ];
+    use HasFactory, InteractsWithMedia;
     protected $guarded = [];
 
     // protected function casts(): array
@@ -32,5 +23,10 @@ class Menu extends Model
     public function category(): BelongsTo
     {
         return $this->belongsTo(Category::class, 'categories_id');
+    }
+
+    public function discount(): BelongsTo
+    {
+        return $this->belongsTo(Discount::class);
     }
 }
