7a8e65e66bf95b02f995d62d34b9455f028cf147
VinaRiqqaK07 <vinariqqakhairiah@gmail.com>
2024-04-20 17:21:37 +0800

    empty cart component

---
+++ resources/views/components/empty-cart.blade.php
@@ -0,0 +1,5 @@
+<section class="h-[85vh] w-full flex flex-col items-center justify-center bg-[#F6FFF2] gap-2">
+    <i class="fa-solid fa-cart-plus mb-4" style="font-size: 100px"></i>
+    <h1 class="font-semibold text-xl">Keranjang kamu masih kosong</h1>
+    <p class="">Yuk, pilih dan pesan menu favoritmu!</p>
+</section>
