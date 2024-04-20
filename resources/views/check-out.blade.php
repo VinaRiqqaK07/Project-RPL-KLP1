cec4ef9f2ade6e08253d6f437b8377e013cd422b
VinaRiqqaK07 <vinariqqakhairiah@gmail.com>
2024-04-20 17:24:21 +0800

    using controller var

--- resources/views/check-out.blade.php
+++ resources/views/check-out.blade.php
@@ -1,80 +1,95 @@
 <x-layouts.app>
-    <x-slot:slot>
-        <div class="h-full">
-            <header class="w-full h-16 flex items-center justify-between bg-orange-500 px-4">
-                <a href="{{ route("order") }}">
-                    <i class="fa-solid fa-arrow-left fa-lg text-white"></i>
-                </a>
-                <h1 class="font-bold text-white text-xl">Keranjang</h1>
+  <x-slot:slot>
+    <div class="h-full">
+      <header class="sticky top-0 flex h-16 w-full items-center justify-between bg-orange-500 px-4">
+        <a href="/order">
+          <i class="fa-solid fa-arrow-left fa-lg text-white"></i>
+        </a>
+        <h1 class="text-xl font-bold text-white">Keranjang</h1>
+        @if(empty($list_pesanan))
+            <i class="fa-solid fa-arrow-rotate-left fa-lg text-white hidden"></i>
+        @else
+            <button id="deleteAll" type="submit">
                 <i class="fa-solid fa-arrow-rotate-left fa-lg text-white"></i>
-            </header>
+            </button>
+        @endif
+      </header>
 
-            <main class="h-auto w-full pb-9 lg:pb-10 mb-12" >
-                <section class="max-h-[83vh] sm:max-h-[85vh] lg:max-h-[80vh] xl:max-h-[90vh] bg-gray-100 overflow-y-auto">
-                    <x-card-order></x-card-order>
-                    <x-card-order></x-card-order>
-                    <x-card-order></x-card-order>
-                    <x-card-order></x-card-order>
-                    <x-card-order></x-card-order>
-                </section>
-            </main>
+      <main class="h-full w-full bg-gray-100">
+        @if(empty($list_pesanan))
+            <x-empty-cart></x-empty-cart>
+        @else
+            <section class="max-h-full overflow-y-auto px-2 pb-4 pt-1">
+                @foreach($list_pesanan as $pesanan)
+                    <x-card-order>
+                        <x-slot:name>{{ $pesanan['name'] }}</x-slot>
+                        <x-slot:price>{{ $pesanan['price'] }}</x-slot>
+                        <x-slot:qty>{{ $pesanan['qty'] }}</x-slot>
+                        <x-slot:image>{{ $pesanan['image'] }}</x-slot>
+                        <x-slot:note>{{ $pesanan['note'] }}</x-slot>
+                    </x-card-order>
+                @endforeach
+                <x-card-order></x-card-order>
+                <x-card-order></x-card-order>
+            </section>
+        @endif
+      </main>
 
-            <footer class="p-4 fixed bottom-0 left-0 w-full bg-white" height="60">
-                <section class="flex flex-row justify-between items-center" >
-                    <section class="flex flex-col">
-                        <p class="text-sm">Jumlah bayar</p>
-                        <p class="text-sm font-bold" >Rp{{ $sumprice ?? 200000 }}</p>
-                    </section>
+      <footer class="fixed bottom-0 w-[400px] bg-white p-4" height="60">
+        <section class="flex flex-row items-center justify-between">
+          <section class="flex flex-col">
+            <p class="text-sm">Jumlah bayar</p>
+            <p class="text-sm font-bold">Rp{{ $sumprice ?? 200000 }}</p>
+          </section>
 
-                    <section class="">
-                        <button id="showModal" class="w-40 py-2 px-4 text-white text-md font-semibold bg-[#70B44E] rounded-lg" >Pesan</button>
-                    </section>
-                </section>
+          <section class="">
+            <button id="showModal" class="text-md w-40 rounded-lg bg-[#70B44E] px-4 py-2 font-semibold text-white">
+              Pesan
+            </button>
+          </section>
+        </section>
 
-                <div id="modalBackdrop" class="hidden fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-50 z-50"></div>
-            </footer>
+        <div id="modalBackdrop" class="fixed left-0 top-0 z-50 hidden h-full w-full bg-gray-500 bg-opacity-50"></div>
+      </footer>
 
-            <div id="deleteModal" class="hidden">
-                <x-delete-modal></x-delete-modal>
-            </div>
+      <div id="deleteModal" class="hidden">
+        <x-delete-modal></x-delete-modal>
+      </div>
 
-            <div id="modalSheet" class="hidden">
-                <x-make-order-modal></x-make-order-modal>
-            </div>
-        </div>
-        
-        
-        <script>
-            document.addEventListener('DOMContentLoaded', () => {
-                var showModalButton = document.getElementById('showModal');
-                var closeModalButton = document.getElementById('closeModal');
-                var modalBackdrop = document.getElementById('modalBackdrop');
-                var modalSheet = document.getElementById('modalSheet');
+      <div id="modalSheet" class="hidden">
+        <x-make-order-modal></x-make-order-modal>
+      </div>
+    </div>
 
-                var showDeleteButton = document.getElementById('deleteButton');
-                var deleteModal = document.getElementById('deleteModal');
-                var closeDeleteButton = document.getElementById('closeDelete');
+    <script>
+      document.addEventListener('DOMContentLoaded', () => {
+        var showModalButton = document.getElementById('showModal');
+        var closeModalButton = document.getElementById('closeModal');
+        var modalBackdrop = document.getElementById('modalBackdrop');
+        var modalSheet = document.getElementById('modalSheet');
 
-                showModalButton.addEventListener('click', () => {
-                    modalBackdrop.classList.remove('hidden');
-                    modalSheet.classList.remove('hidden');
-                });
-                closeModalButton.addEventListener('click', () => {
-                    modalSheet.classList.add('hidden');
-                    modalBackdrop.classList.add('hidden');
-                });
+        var showDeleteButton = document.getElementById('deleteButton');
+        var deleteModal = document.getElementById('deleteModal');
+        var closeDeleteButton = document.getElementById('closeDelete');
 
-                showDeleteButton.addEventListener('click', () => {
-                    deleteModal.classList.remove('hidden');
-                    modalBackdrop.classList.remove('hidden');
-                });
-                closeDeleteButton.addEventListener('click', () => {
-                    deleteModal.classList.add('hidden');
-                    modalBackdrop.classList.add('hidden');
-                });
+        showModalButton.addEventListener('click', () => {
+          modalBackdrop.classList.remove('hidden');
+          modalSheet.classList.remove('hidden');
+        });
+        closeModalButton.addEventListener('click', () => {
+          modalSheet.classList.add('hidden');
+          modalBackdrop.classList.add('hidden');
+        });
 
-            });
-        </script>
-    </x-slot>
+        showDeleteButton.addEventListener('click', () => {
+          deleteModal.classList.remove('hidden');
+          modalBackdrop.classList.remove('hidden');
+        });
+        closeDeleteButton.addEventListener('click', () => {
+          deleteModal.classList.add('hidden');
+          modalBackdrop.classList.add('hidden');
+        });
+      });
+    </script>
+  </x-slot>
 </x-layouts.app>
-
