608b3007b70beed165d2aabc02a28b8e8741a12d
VinaRiqqaK07 <vinariqqakhairiah@gmail.com>
2024-05-23 02:53:56 +0800

    feat. make order

--- resources/views/components/make-order-modal.blade.php
+++ resources/views/components/make-order-modal.blade.php
@@ -5,14 +5,15 @@
             <button id="closeModal"><i class="fa-solid fa-circle-xmark fa-xl"></i></button>
             
         </section>
-        <section class="flex flex-col my-1 pb-2 gap-2">
-            <label class="font-semibold my-1">Nama Pemesan</label>
-            <input type="text" placeholder="Masukkan nama Anda" class="px-2 py-3 focus:outline-none focus:ring focus:border-blue-300 rounded-lg bg-slate-100 " />
-            <label class="font-semibold mt-3">Nomor Meja</label>
-            <input type="text" placeholder="Masukkan nomor meja" class="px-2 py-3 focus:outline-none focus:ring focus:border-blue-300 rounded-lg bg-slate-100 " />
-            <a href="/order/checkout/payment">
-                <button class="w-full py-3 px-4 mt-4 text-white text-md font-semibold bg-[#70B44E] rounded-lg">Pesan</button>
-            </a>
-        </section>   
+        <form action="{{ route('order.place') }}" method="POST" >
+            @csrf
+            <section class="flex flex-col my-1 pb-2 gap-2">
+                <label class="font-semibold my-1">Nama Pemesan</label>
+                <input type="text" name="customer_name" placeholder="Masukkan nama Anda" class="px-2 py-3 focus:outline-none focus:ring focus:border-blue-300 rounded-lg bg-slate-100 " required/>
+                <label class="font-semibold mt-3">Nomor Meja</label>
+                <input type="text" name="table_number" placeholder="Masukkan nomor meja" class="px-2 py-3 focus:outline-none focus:ring focus:border-blue-300 rounded-lg bg-slate-100 " required/>    
+                <button type="submit" class="w-full py-3 px-4 mt-4 text-white text-md font-semibold bg-[#70B44E] rounded-lg">Pesan</button>
+            </section>   
+        </form>
     </footer>
 </section>
