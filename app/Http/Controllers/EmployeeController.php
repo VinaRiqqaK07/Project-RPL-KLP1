442583f06775cef6060374a64cd79f75cd134c01
VinaRiqqaK07 <vinariqqakhairiah@gmail.com>
2024-05-23 02:54:17 +0800

    first employee commit

---
+++ app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Menu;
+use App\Models\Orders;
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\DB;
+
+class EmployeeController extends Controller
+{
+    //
+    public function index()
+    {
+        $menus = Menu::all();
+        $orderList = Orders::all();
+        return view('employee.main-page', [
+            'menus' => $menus,
+            'orderList' => $orderList,
+        ]);
+    }
+
+}
