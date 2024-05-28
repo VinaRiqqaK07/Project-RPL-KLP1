84b436fcb06a15d98466e50f43b703a8ca339c3a
rveralda03 <ruthveralda163@gmail.com>
2024-05-29 01:36:53 +0800

    tambahkan login karyawan dan transaction modals

--- resources/views/components/employee-components/transaction-modal-v3.blade.php
+++ resources/views/components/employee-components/transaction-modal-v3.blade.php
@@ -10,43 +10,42 @@
           <p class="mb-1">Order Detail :</p>
           <section class="flex gap-3 py-1">
             <x-employee-components.category-employee>
-              <x-slot:active>false</x-slot>
-              <x-slot:icon>fa-solid fa-bowl-food</x-slot>
-              <x-slot:name>Food</x-slot>
+              <x-slot:active>true</x-slot>
+              <x-slot:icon>fa-solid fa-utensils</x-slot>
+              <x-slot:name>Dine-In</x-slot>
             </x-employee-components.category-employee>
             <x-employee-components.category-employee>
               <x-slot:active>false</x-slot>
-              <x-slot:icon>fa-solid fa-mug-saucer</x-slot>
-              <x-slot:name>Drink</x-slot>
+              <x-slot:icon>fa-solid fa-box</x-slot>
+              <x-slot:name>Take Away</x-slot>
             </x-employee-components.category-employee>
           </section>
-          <section class="flex">
-            <section class="flex flex-col py-1 pe-1" style="width: 50%">
-              <label class="my-1 font-medium">Customer Name</label>
-              <input
-                type="text"
-                placeholder="Type name"
-                class="rounded-lg bg-slate-100 px-2 py-1 focus:border-blue-300 focus:outline-none focus:ring"
-              />
-            </section>
-            <section class="flex flex-col py-1 pe-1" style="width: 50%">
-              <label class="my-1 font-medium">Table Number</label>
-              <input
-                type="number"
-                placeholder="Type number"
-                class="rounded-lg bg-slate-100 px-2 py-1 focus:border-blue-300 focus:outline-none focus:ring"
-              />
-            </section>
+          <section class="py-1">
+            <label class="my-1 font-medium">Customer Name</label>
+            <input
+              type="text"
+              placeholder="Type customer name"
+              class="rounded-lg bg-slate-100 px-2 py-1 focus:border-blue-300 focus:outline-none focus:ring"
+            />
           </section>
           <section class="py-1">
-            <label>Menu selected (n item)</label>
+            <label>Menu selected (2 items)</label>
             <section class="flex justify-between pe-1">
               <section class="flex gap-2">
                 <p class="font-thin">nx</p>
-                <p>{{ $name_menu ?? "NASI GORENG" }}</p>
+                <p>NASI GORENG</p>
               </section>
               <section>
-                <p>Rp{{ $qty_price ?? number_format(1000000) }}</p>
+                <p>Rp3.000.000,00</p>
+              </section>
+            </section>
+            <section class="flex justify-between pe-1">
+              <section class="flex gap-2">
+                <p class="font-thin">nx</p>
+                <p>NASI LAIN</p>
+              </section>
+              <section>
+                <p>Rp200.00</p>
               </section>
             </section>
           </section>
@@ -55,7 +54,7 @@
           <p>Payment Detail</p>
           <section class="flex gap-3 py-1">
             <x-employee-components.category-employee>
-              <x-slot:active>false</x-slot>
+              <x-slot:active>true</x-slot>
               <x-slot:icon>fa-solid fa-money-bill-wave</x-slot>
               <x-slot:name>Cash</x-slot>
             </x-employee-components.category-employee>
@@ -68,14 +67,8 @@
           <section>
             <p class="text-sm">Quick Access</p>
             <section class="flex gap-3 py-1">
-              <x-employee-components.category-employee>
-                <x-slot:active>false</x-slot>
-                <x-slot:name>Uang Pas</x-slot>
-              </x-employee-components.category-employee>
-              <x-employee-components.category-employee>
-                <x-slot:active>false</x-slot>
-                <x-slot:name>50.000</x-slot>
-              </x-employee-components.category-employee>
+              <button class="rounded-lg bg-slate-200 px-2 py-1">50.000</button>
+              <button class="rounded-lg bg-slate-200 px-2 py-1">100.000</button>
             </section>
           </section>
           <section class="flex">
@@ -87,31 +80,19 @@
                 class="rounded-lg bg-slate-100 px-2 py-1 focus:border-blue-300 focus:outline-none focus:ring"
               />
             </section>
-            <section class="flex flex-col py-1" style="width: 50%">
-              <label class="my-1 font-medium">Pay Amount</label>
-              <input
-                type="number"
-                placeholder="Type pay amount"
-                class="rounded-lg bg-slate-100 px-2 py-1 focus:border-blue-300 focus:outline-none focus:ring"
-              />
-            </section>
           </section>
           <section class="flex flex-col gap-1 py-2">
             <section class="flex justify-between">
               <p class="text-sm">Sub Total</p>
-              <p class="text-sm">Rp{{ $total ?? number_format(3200000) }}</p>
+              <p class="text-sm">Rp3.200.000,00</p>
             </section>
             <section class="flex justify-between">
               <p class="text-sm">Discount</p>
-              <p class="text-sm">{{ ($discount ?? 0.15) * 100 }}%</p>
+              <p class="text-sm">50%</p>
             </section>
             <section class="flex justify-between">
               <p>Total</p>
-              <p>Rp{{ $total ?? number_format(3200000) }}</p>
-            </section>
-            <section class="flex justify-between">
-              <p>Change</p>
-              <p>Rp{{ $total ?? number_format(0) }}</p>
+              <p>Rp1.600.000,00</p>
             </section>
             <button class="text-md mt-4 w-full rounded-lg bg-[#70B44E] px-1 py-2 font-semibold text-white">Pay</button>
             <button class="text-md mt-2 w-full rounded-lg bg-gray-200 px-1 py-2 font-semibold text-black">
