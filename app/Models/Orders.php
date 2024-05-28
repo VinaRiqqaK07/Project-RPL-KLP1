02ce672c5a33a1e6bff4a06a00a437aaaff1aaa0
VinaRiqqaK07 <vinariqqakhairiah@gmail.com>
2024-05-29 01:42:31 +0800

    minor correction

--- app/Models/Orders.php
+++ app/Models/Orders.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Orders extends Model
 {
@@ -21,7 +22,12 @@ class Orders extends Model
 
     // Hidden from JSON
     protected $hidden = [
-        'order_id', 'discoung_id',
+        'order_id', 'discount_id',
         'created_at', 'updated_at',
     ];
+
+    public function discount(): BelongsTo
+    {
+        return $this->belongsTo(Discount::class);
+    }
 }
